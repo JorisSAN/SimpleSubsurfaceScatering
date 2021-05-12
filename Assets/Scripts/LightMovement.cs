694ebf0480d7d0aa742ab34ad9f53e7c78a51f5c
Joris Sanchez <sancho.joris@gmail.com>
2021-05-12 14:32:40 +0200

    ajout du projet unity avec le shader SSS

---
+++ Assets/Scripts/LightMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Sin(Time.time) * 1.5f;
+        float y = transform.position.y;
+        float z = transform.position.z;
+        transform.position = new Vector3(x, y, z);
+    }
+}
